4d4ba3d101dc7e9af067a868099fbfad23852f4d
Julio Santos <julioc.9730@gmail.com>
2023-10-08 22:20:14 -0300

    Update searchPokemons.cy.js

--- cypress/e2e/searchPokemons.cy.js
+++ cypress/e2e/searchPokemons.cy.js
@@ -2,6 +2,7 @@ import { randomPokemons } from '../support/randomMass';
 
 describe('Pesquisando Pokemons', () => {
     const pokemon = randomPokemons();
+    const nonExistentPokemon = 'its_not_pokemon';
 
     it('Buscando um pokemon aleatório na barra de busca', () => {
         cy.intercept(`**/pokemon/${pokemon.toLowerCase()}`).as('wtdPokemon');
@@ -20,8 +21,6 @@ describe('Pesquisando Pokemons', () => {
     });
 
     it('Procurando um pokemon que não existe', () => {
-        const nonExistentPokemon = 'its_not_pokemon';
-
         cy.get('#js-input-search').as('search');
         cy.get('@search').scrollIntoView();
         cy.get('@search').type(`${nonExistentPokemon}{enter}`, { delay: 80 });
