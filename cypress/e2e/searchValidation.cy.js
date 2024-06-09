8aa622da53abc335109b5e2d7c1605719e0cf911
Julio Santos <julioc.9730@gmail.com>
2024-06-08 22:54:32 -0300

    Atualizar o searchValidation.cy.js

--- cypress/e2e/searchValidation.cy.js
+++ cypress/e2e/searchValidation.cy.js
@@ -1,24 +1,28 @@
 import { randomPokemons } from '../support/randomMass';
 
 describe('Validação de busca', () => {
-    const pokemon = randomPokemons();
+    
+    afterEach(() => cy.screenshot());
 
     it('Confirmando que o pokémon procurado é exibido corretamente', () => {
+        const pokemon = randomPokemons();
+        
         cy.intercept(`**/pokemon/${pokemon.toLowerCase()}`).as('wtdPokemon');
-
         cy.get('#js-input-search').type(`${pokemon}{enter}`, { delay: 80 });
         cy.wait('@wtdPokemon');
 
         cy.contains('.card-pokemon', pokemon)
             .should('be.visible')
-            .and('have.length', 1)
-            .screenshot();
+            .and('have.length', 1);
     });
 
     it('Verificando a busca por um pokémon inexistente', () => {
         cy.get('#js-input-search').type('non-existent_pokemon{enter}', { delay: 80 });
-        cy.get('strong.js-count-pokemons').should('not.be.empty').and('have.text', '0');
-        cy.get('.card-pokemon').should('not.exist');
-        cy.screenshot();
+        cy.get('strong.js-count-pokemons')
+            .should('not.be.empty')
+            .and('have.text', '0');
+            
+        cy.get('.card-pokemon')
+            .should('not.exist');
     });
 });
